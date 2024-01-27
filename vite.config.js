0cf7a7c9bec6ca2834024477a13cb1ffebbe5118
madzals <jmadiedor@unicartagena.edu.co>
2024-01-27 09:28:11 -0500

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/madzals/react-deploy-example",
+  base: "https://madzals.github.io/react-tasks-example/",
 });
